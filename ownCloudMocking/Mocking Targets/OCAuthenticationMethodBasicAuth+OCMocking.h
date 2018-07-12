bbab2588435d8e52dc67aa990bccf9f715e711d9
Felix Schwarz <felix.schwarz@iospirit.com>
2018-07-12 11:19:06 +0200

    - Add mocking support for OCAuthenticationMethodBasicAuth class

---
+++ ownCloudMocking/Mocking Targets/OCAuthenticationMethodBasicAuth+OCMocking.h
@@ -0,0 +1,46 @@
+//
+//  OCAuthenticationMethodBasicAuth+OCMocking.h
+//  ownCloudSDK
+//
+//  Created by Felix Schwarz on 12.07.18.
+//  Copyright © 2018 ownCloud GmbH. All rights reserved.
+//
+
+/*
+ * Copyright (C) 2018, ownCloud GmbH.
+ *
+ * This code is covered by the GNU Public License Version 3.
+ *
+ * For distribution utilizing Apple mechanisms please see https://owncloud.org/contribute/iOS-license-exception/
+ * You should have received a copy of this license along with this program. If not, see <http://www.gnu.org/licenses/gpl-3.0.en.html>.
+ *
+ */
+
+#import <ownCloudSDK/ownCloudSDK.h>
+#import "OCMockManager.h"
+
+@interface OCAuthenticationMethodBasicAuth (OCMocking)
+
+// Counterparts of mockable methods
++ (void)ocm_ba_detectAuthenticationMethodSupportForConnection:(OCConnection *)connection withServerResponses:(NSDictionary<NSURL *, OCConnectionRequest *> *)serverResponses options:(OCAuthenticationMethodDetectionOptions)options completionHandler:(void(^)(OCAuthenticationMethodIdentifier identifier, BOOL supported))completionHandler;
+
+- (void)ocm_ba_generateBookmarkAuthenticationDataWithConnection:(OCConnection *)connection options:(OCAuthenticationMethodBookmarkAuthenticationDataGenerationOptions)options completionHandler:(void(^)(NSError *error, OCAuthenticationMethodIdentifier authenticationMethodIdentifier, NSData *authenticationData))completionHandler;
+
+- (void)ocm_ba_authenticateConnection:(OCConnection *)connection withCompletionHandler:(OCAuthenticationMethodAuthenticationCompletionHandler)completionHandler;
+
+- (void)ocm_ba_deauthenticateConnection:(OCConnection *)connection withCompletionHandler:(OCAuthenticationMethodAuthenticationCompletionHandler)completionHandler;
+
+@end
+
+// Block and mock location for every mockable method
+typedef void(^OCMockAuthenticationMethodBasicAuthDetectAuthenticationMethodSupportForConnectionBlock)(OCConnection *connection, NSDictionary<NSURL *, OCConnectionRequest *> *serverResponses, OCAuthenticationMethodDetectionOptions options, void(^completionHandler)(OCAuthenticationMethodIdentifier identifier, BOOL supported));
+extern OCMockLocation OCMockLocationAuthenticationMethodBasicAuthDetectAuthenticationMethodSupportForConnection;
+
+typedef void(^OCMockAuthenticationMethodBasicAuthGenerateBookmarkAuthenticiationDataWithConnectionBlock)(OCConnection *connection, OCAuthenticationMethodBookmarkAuthenticationDataGenerationOptions options, void(^completionHandler)(NSError *error, OCAuthenticationMethodIdentifier authenticationMethodIdentifier, NSData *authenticationData));
+extern OCMockLocation OCMockLocationAuthenticationMethodBasicAuthGenerateBookmarkAuthenticiationDataWithConnection;
+
+typedef void(^OCMockAuthenticationMethodBasicAuthAuthenticateConnectionBlock)(OCConnection *connection, OCAuthenticationMethodAuthenticationCompletionHandler completionHandler);
+extern OCMockLocation OCMockLocationAuthenticationMethodBasicAuthAuthenticateConnection;
+
+typedef void(^OCMockAuthenticationMethodBasicAuthDeauthenticateConnectionBlock)(OCConnection *connection, OCAuthenticationMethodAuthenticationCompletionHandler completionHandler);
+extern OCMockLocation OCMockLocationAuthenticationMethodBasicAuthDeauthenticateConnection;
---
+++ ownCloudMocking/Mocking Targets/OCAuthenticationMethodBasicAuth+OCMocking.m
@@ -0,0 +1,104 @@
+//
+//  OCAuthenticationMethodBasicAuth+OCMocking.m
+//  ownCloudSDK
+//
+//  Created by Felix Schwarz on 12.07.18.
+//  Copyright © 2018 ownCloud GmbH. All rights reserved.
+//
+
+/*
+ * Copyright (C) 2018, ownCloud GmbH.
+ *
+ * This code is covered by the GNU Public License Version 3.
+ *
+ * For distribution utilizing Apple mechanisms please see https://owncloud.org/contribute/iOS-license-exception/
+ * You should have received a copy of this license along with this program. If not, see <http://www.gnu.org/licenses/gpl-3.0.en.html>.
+ *
+ */
+
+#import "OCAuthenticationMethodBasicAuth+OCMocking.h"
+#import "NSObject+OCSwizzle.h"
+
+@implementation OCAuthenticationMethodBasicAuth (OCMocking)
+
++ (void)load
+{
+	[self addMockLocation:OCMockLocationAuthenticationMethodBasicAuthDetectAuthenticationMethodSupportForConnection
+	      forClassSelector:@selector(detectAuthenticationMethodSupportForConnection:withServerResponses:options:completionHandler:)
+	      with:@selector(ocm_ba_detectAuthenticationMethodSupportForConnection:withServerResponses:options:completionHandler:)];
+
+	[self addMockLocation:OCMockLocationAuthenticationMethodBasicAuthGenerateBookmarkAuthenticiationDataWithConnection
+	      forSelector:@selector(generateBookmarkAuthenticationDataWithConnection:options:completionHandler:)
+	      with:@selector(ocm_ba_generateBookmarkAuthenticationDataWithConnection:options:completionHandler:)];
+
+	[self addMockLocation:OCMockLocationAuthenticationMethodBasicAuthAuthenticateConnection
+	      forSelector:@selector(authenticateConnection:withCompletionHandler:)
+	      with:@selector(ocm_ba_authenticateConnection:withCompletionHandler:)];
+
+	[self addMockLocation:OCMockLocationAuthenticationMethodBasicAuthDeauthenticateConnection
+	      forSelector:@selector(deauthenticateConnection:withCompletionHandler:)
+	      with:@selector(ocm_ba_deauthenticateConnection:withCompletionHandler:)];
+}
+
++ (void)ocm_ba_detectAuthenticationMethodSupportForConnection:(OCConnection *)connection withServerResponses:(NSDictionary<NSURL *, OCConnectionRequest *> *)serverResponses options:(OCAuthenticationMethodDetectionOptions)options completionHandler:(void(^)(OCAuthenticationMethodIdentifier identifier, BOOL supported))completionHandler
+{
+	OCMockAuthenticationMethodBasicAuthDetectAuthenticationMethodSupportForConnectionBlock mockBlock;
+
+	if ((mockBlock = [[OCMockManager sharedMockManager] mockingBlockForLocation:OCMockLocationAuthenticationMethodBasicAuthDetectAuthenticationMethodSupportForConnection]) != nil)
+	{
+		mockBlock(connection, serverResponses, options, completionHandler);
+	}
+	else
+	{
+		[self ocm_ba_detectAuthenticationMethodSupportForConnection:connection withServerResponses:serverResponses options:options completionHandler:completionHandler];
+	}
+}
+
+- (void)ocm_ba_generateBookmarkAuthenticationDataWithConnection:(OCConnection *)connection options:(OCAuthenticationMethodBookmarkAuthenticationDataGenerationOptions)options completionHandler:(void(^)(NSError *error, OCAuthenticationMethodIdentifier authenticationMethodIdentifier, NSData *authenticationData))completionHandler
+{
+	OCMockAuthenticationMethodBasicAuthGenerateBookmarkAuthenticiationDataWithConnectionBlock mockBlock;
+
+	if ((mockBlock = [[OCMockManager sharedMockManager] mockingBlockForLocation:OCMockLocationAuthenticationMethodBasicAuthGenerateBookmarkAuthenticiationDataWithConnection]) != nil)
+	{
+		mockBlock(connection, options, completionHandler);
+	}
+	else
+	{
+		[self ocm_ba_generateBookmarkAuthenticationDataWithConnection:connection options:options completionHandler:completionHandler];
+	}
+}
+
+- (void)ocm_ba_authenticateConnection:(OCConnection *)connection withCompletionHandler:(OCAuthenticationMethodAuthenticationCompletionHandler)completionHandler;
+{
+	OCMockAuthenticationMethodBasicAuthAuthenticateConnectionBlock mockBlock;
+
+	if ((mockBlock = [[OCMockManager sharedMockManager] mockingBlockForLocation:OCMockLocationAuthenticationMethodBasicAuthAuthenticateConnection]) != nil)
+	{
+		mockBlock(connection, completionHandler);
+	}
+	else
+	{
+		[self ocm_ba_authenticateConnection:connection withCompletionHandler:completionHandler];
+	}
+}
+
+- (void)ocm_ba_deauthenticateConnection:(OCConnection *)connection withCompletionHandler:(OCAuthenticationMethodAuthenticationCompletionHandler)completionHandler;
+{
+	OCMockAuthenticationMethodBasicAuthDeauthenticateConnectionBlock mockBlock;
+
+	if ((mockBlock = [[OCMockManager sharedMockManager] mockingBlockForLocation:OCMockLocationAuthenticationMethodBasicAuthDeauthenticateConnection]) != nil)
+	{
+		mockBlock(connection, completionHandler);
+	}
+	else
+	{
+		[self ocm_ba_deauthenticateConnection:connection withCompletionHandler:completionHandler];
+	}
+}
+
+@end
+
+OCMockLocation OCMockLocationAuthenticationMethodBasicAuthDetectAuthenticationMethodSupportForConnection = @"OCAuthenticationMethodBasicAuth.detectAuthenticationMethodSupportForConnection";
+OCMockLocation OCMockLocationAuthenticationMethodBasicAuthGenerateBookmarkAuthenticiationDataWithConnection = @"OCAuthenticationMethodBasicAuth.generateBookmarkAuthenticationDataWithConnection";
+OCMockLocation OCMockLocationAuthenticationMethodBasicAuthAuthenticateConnection = @"OCAuthenticationMethodBasicAuth.authenticateConnection";
+OCMockLocation OCMockLocationAuthenticationMethodBasicAuthDeauthenticateConnection = @"OCAuthenticationMethodBasicAuth.deauthenticateConnection";
