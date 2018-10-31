4e0328773fe8e2b889422ffd4eb9035070cca93d
Javier Gonzalez <javiergonzper@gmail.com>
2018-10-31 08:38:07 +0100

    Added a new mocked method on the class OCConnection

--- ownCloudMocking/Mocking Targets/OCConnection+OCMocking.m
+++ ownCloudMocking/Mocking Targets/OCConnection+OCMocking.m
@@ -16,6 +16,9 @@
 	[self addMockLocation:OCMockLocationOCConnectionPrepareForSetupWithOptions
 		 forSelector:@selector(prepareForSetupWithOptions:completionHandler:)
 					 with:@selector(ocm_prepareForSetupWithOptions:completionHandler:)];
+	[self addMockLocation:OCMockLocationOCConnectionGenerateAuthenticationDataWithMethod
+			  forSelector:@selector(generateAuthenticationDataWithMethod:options:completionHandler:)
+					 with:@selector(ocm_generateAuthenticationDataWithMethod:options:completionHandler:)];
 }
 
 - (void)ocm_prepareForSetupWithOptions:(NSDictionary<NSString *, id> *)options completionHandler:(void(^)(OCConnectionIssue *issue, NSURL *suggestedURL, NSArray <OCAuthenticationMethodIdentifier> *supportedMethods, NSArray <OCAuthenticationMethodIdentifier> *preferredAuthenticationMethods))completionHandler
@@ -32,6 +35,21 @@
 	}
 }
 
+- (void)ocm_generateAuthenticationDataWithMethod:(OCAuthenticationMethodIdentifier)methodIdentifier options:(OCAuthenticationMethodBookmarkAuthenticationDataGenerationOptions)options completionHandler:(void(^)(NSError *error, OCAuthenticationMethodIdentifier authenticationMethodIdentifier, NSData *authenticationData))completionHandler
+{
+	OCMockOCConnectionGenerateAuthenticationDataWithMethodBlock mockBlock;
+
+	if ((mockBlock = [[OCMockManager sharedMockManager] mockingBlockForLocation:OCMockLocationOCConnectionGenerateAuthenticationDataWithMethod]) != nil)
+	{
+		mockBlock(methodIdentifier, options, completionHandler);
+	}
+	else
+	{
+		[self ocm_generateAuthenticationDataWithMethod:methodIdentifier options:options completionHandler:completionHandler];
+	}
+}
+
 OCMockLocation OCMockLocationOCConnectionPrepareForSetupWithOptions = @"OCConnection.prepareForSetupWithOptions";
+OCMockLocation OCMockLocationOCConnectionGenerateAuthenticationDataWithMethod = @"OCConnection.generateAuthenticationDataWithMethod";
 
 @end
