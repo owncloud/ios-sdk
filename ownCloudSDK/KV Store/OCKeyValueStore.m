de3a36137d55b3069a06209691a8ef82d3e7e551
Felix Schwarz <felix.schwarz@iospirit.com>
2019-09-11 12:59:21 +0200

    - Extend set of secure classes returned by OCEvent.safeClasses - Re-add explicit secure coding enforcement to OCKeyValueStore since it doesn't seem to be possible to turn it off that simply (=> https://developer.apple.com/documentation/foundation/nskeyedunarchiver/1410824-requiressecurecoding for details)

--- ownCloudSDK/KV Store/OCKeyValueStore.m
+++ ownCloudSDK/KV Store/OCKeyValueStore.m
@@ -505,15 +505,16 @@ typedef NSMutableDictionary<OCKeyValueStoreKey, OCKeyValueRecord *> * OCKeyValue
 		if ((data = [[NSData alloc] initWithContentsOfURL:url]) != nil)
 		{
 			NSError *error = nil;
-			// Temporarily remove secure coding checks until all secure coding compliant OC classes have unit tests verifiying their secure coding implementations does not loose any data
-			// storeContents = [NSKeyedUnarchiver unarchivedObjectOfClasses:[[NSSet alloc] initWithObjects:[NSMutableDictionary class], [OCKeyValueRecord class], [NSString class], nil] fromData:data error:&error];
-			storeContents = [NSKeyedUnarchiver unarchiveTopLevelObjectWithData:data error:&error];
+			storeContents = [NSKeyedUnarchiver unarchivedObjectOfClasses:[[NSSet alloc] initWithObjects:[NSMutableDictionary class], [OCKeyValueRecord class], [NSString class], nil] fromData:data error:&error];
 
-			if ((storeContents != nil) && ![storeContents isKindOfClass:[NSMutableDictionary class]])
-			{
-				OCLogError(@"Store contents not a NSMutableDictionary, but instead a %@", NSStringFromClass(storeContents.class));
-				storeContents = [NSMutableDictionary new];
-			}
+			// ~~ Temporarily remove secure coding checks until all secure coding compliant OC classes have unit tests verifiying their secure coding implementations does not loose any data ~~
+			// storeContents = [NSKeyedUnarchiver unarchiveTopLevelObjectWithData:data error:&error];
+			//
+			// if ((storeContents != nil) && ![storeContents isKindOfClass:[NSMutableDictionary class]])
+			// {
+			// 	OCLogError(@"Store contents not a NSMutableDictionary, but instead a %@", NSStringFromClass(storeContents.class));
+			// 	storeContents = [NSMutableDictionary new];
+			// }
 
 			if (storeContents == nil)
 			{
