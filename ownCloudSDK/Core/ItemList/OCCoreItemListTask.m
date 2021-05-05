ef04bccc6b0172cc371b4cf4aaf13f372ce938bc
Matthias Hühne <mhuehne@owncloud.com>
2021-05-05 09:19:27 +0200

    Translation sync (#83)
    * [tx] updated from transifex
    
    * Removed the es lang because was wrong and it is not necessary to map it (es -> es) (#43)
    
    * [tx] updated from transifex
    
    * [tx] updated from transifex
    
    * [tx] updated from transifex
    
    * [tx] updated from transifex
    
    * [tx] updated from transifex
    
    * [tx] updated from transifex
    
    * [tx] updated from transifex
    
    * [tx] updated from transifex
    
    * [tx] updated from transifex
    
    * [tx] updated from transifex
    
    * [tx] updated from transifex
    
    * [tx] updated from transifex
    
    * [tx] updated from transifex
    
    * [tx] updated from transifex
    
    * [tx] updated from transifex
    
    * [tx] updated from transifex
    
    * [tx] updated from transifex
    
    * [tx] updated from transifex
    
    * [tx] updated from transifex
    
    * [tx] updated from transifex
    
    * [tx] updated from transifex
    
    * [tx] updated from transifex
    
    * [tx] updated from transifex
    
    * [tx] updated from transifex
    
    * [tx] updated from transifex
    
    * [tx] updated from transifex
    
    * [tx] updated from transifex
    
    * [tx] updated from transifex
    
    * [tx] updated from transifex
    
    * [tx] updated from transifex
    
    * [tx] updated from transifex
    
    * removed duplicated german localization (no longer needed, because salutation "Du" is no longer used)
    
    * [tx] updated from transifex
    
    * [tx] updated from transifex
    
    * added missing localization strings
    
    * [tx] updated from transifex
    
    * [tx] updated from transifex
    
    * [tx] updated from transifex
    
    Co-authored-by: ownClouders <devops@owncloud.com>
    Co-authored-by: Javier Gonzalez <javiergonzper@users.noreply.github.com>
    Co-authored-by: Dominik Schmidt <domme@tomahawk-player.org>
    Co-authored-by: Thomas Müller <thomas.mueller@tmit.eu>
    Co-authored-by: Michael Barz <mbarz@owncloud.com>
    Co-authored-by: Benedikt Kulmann <benedikt@kulmann.biz>
    Co-authored-by: Jan Ackermann <jackermann@owncloud.com>

--- ownCloudSDK/Core/ItemList/OCCoreItemListTask.m
+++ ownCloudSDK/Core/ItemList/OCCoreItemListTask.m
@@ -406,7 +406,7 @@
 
 - (OCActivity *)provideActivity
 {
-	OCActivity *activity = [OCActivity withIdentifier:self.activityIdentifier description:[NSString stringWithFormat:@"Retrieving items for %@", self.path] statusMessage:nil ranking:0];
+	OCActivity *activity = [OCActivity withIdentifier:self.activityIdentifier description:[NSString stringWithFormat:OCLocalized(@"Retrieving items for %@"), self.path] statusMessage:nil ranking:0];
 
 	activity.progress = NSProgress.indeterminateProgress;
 
