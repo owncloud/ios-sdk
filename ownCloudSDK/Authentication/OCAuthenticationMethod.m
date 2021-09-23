f400e1280c4f927de36afc0563a41031a0e9b0be
Felix Schwarz <fs-git@iospirit.com>
2021-09-23 12:44:01 +0200

    - Browser Session Class: add AWBrowser to simplify configuration for AirWatch browser

--- ownCloudSDK/Authentication/OCAuthenticationMethod.m
+++ ownCloudSDK/Authentication/OCAuthenticationMethod.m
@@ -342,7 +342,8 @@ INCLUDE_IN_CLASS_SETTINGS_SNAPSHOTS(OCAuthenticationMethod)
 				@"operating-system" : @"Use ASWebAuthenticationSession for browser sessions.",
 				@"UIWebView" : @"Use UIWebView for browser sessions. Requires compilation with `OC_FEATURE_AVAILABLE_UIWEBVIEW_BROWSER_SESSION=1` preprocessor flag.",
 				@"CustomScheme" : @"Replace http and https with custom schemes to delegate browser sessions to a different app.",
-				@"MIBrowser" : @"Replace `http` with `mibrowser` and `https` with `mibrowsers` to delegate browser sessions to the MobileIron browser."
+				@"MIBrowser" : @"Replace `http` with `mibrowser` and `https` with `mibrowsers` to delegate browser sessions to the MobileIron browser.",
+				@"AWBrowser" : @"Replace `http` with `awb` and `https` with `awbs` to delegate browser sessions to the AirWatch browser."
 			},
 			OCClassSettingsMetadataKeyCategory	: @"Authentication"
 		},
