96b5e54a742f2cbaa78ddbcb3b1282a0b23193d3
Felix Schwarz <fs-git@iospirit.com>
2022-04-08 11:57:26 +0200

    - remove OCDataCellConfiguration (replaced by CollectionViewCellConfiguration in ownCloudAppShared)

--- ownCloudSDK/ownCloudSDK.h
+++ ownCloudSDK/ownCloudSDK.h
@@ -195,8 +195,6 @@ FOUNDATION_EXPORT const unsigned char ownCloudSDKVersionString[];
 #import <ownCloudSDK/OCDataConverterPipeline.h>
 #import <ownCloudSDK/OCDataItemPresentable.h>
 #import <ownCloudSDK/OCDataRenderer.h>
-#import <ownCloudSDK/OCDataItemCellConfiguration.h>
-#import <ownCloudSDK/NSObject+OCDataItemCellConfiguration.h>
 
 #import <ownCloudSDK/OCCore+DataSources.h>
 
