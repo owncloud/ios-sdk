ad4b787314278935f85c97848bca9cc7643b31ac
Felix Schwarz <fs-git@iospirit.com>
2024-11-28 10:27:18 +0100

    - add support for OpenAPI's "allOf" (https://swagger.io/docs/specification/v3_0/data-models/oneof-anyof-allof-not/#allof) references

--- tools/ocapigen/Code Generation/Schema/OCSchema.m
+++ tools/ocapigen/Code Generation/Schema/OCSchema.m
@@ -19,9 +19,21 @@
 #import "OCSchema.h"
 #import "OCSchemaProperty.h"
 
+@interface OCYAMLNode (Requirements)
+@end
+
+@implementation OCYAMLNode (Requirements)
+
+- (NSArray<NSString *> *)requiredProperties
+{
+	return OCTypedCast(self.childrenByName[@"required"].value, NSArray);
+}
+
+@end
+
 @implementation OCSchema
 
-- (instancetype)initWithYAMLNode:(OCYAMLNode *)node
+- (instancetype)initWithYAMLNode:(OCYAMLNode *)node parser:(OCYAMLParser *)parser
 {
 	if ((self = [super init]) != nil)
 	{
@@ -32,42 +44,59 @@
 
 		self.properties = [NSMutableArray new];
 
-		for (OCYAMLNode *propertyNode in node.childrenByName[@"properties"].children)
-		{
-			OCSchemaProperty *property = [OCSchemaProperty new];
+		[self addPropertiesFrom:node additionalRequired:nil];
 
-			property.schema = self;
-			property.yamlNode= propertyNode;
-
-			property.name = propertyNode.name;
-			property.desc = propertyNode.childrenByName[@"description"].value;
-			property.type = propertyNode.childrenByName[@"type"].value;
-			property.required = [OCTypedCast(node.childrenByName[@"required"].value,NSArray) containsObject:propertyNode.name];
-			property.format = propertyNode.childrenByName[@"format"].value;
-			property.pattern = propertyNode.childrenByName[@"pattern"].value;
-
-			if (property.type == nil)
-			{
-				property.type = propertyNode.childrenByName[@"$ref"].value;
-			}
-
-			if ([property.type isEqual:@"array"] && (propertyNode.childrenByName[@"items"] != nil))
-			{
-				OCSchemaPropertyType itemType = propertyNode.childrenByName[@"items"].childrenByName[@"$ref"].value;
-
-				if (itemType == nil)
+		if (node.childrenByName[@"allOf"]) {
+			NSLog(@"AllOf: %@", node.childrenByName[@"allOf"].value);
+			for (NSString *reference in OCTypedCast(node.childrenByName[@"allOf"].value, NSArray)) {
+				OCYAMLNode *referencedNode;
+				if ((referencedNode = [parser nodeForReference:reference]) != nil)
 				{
-					itemType = propertyNode.childrenByName[@"items"].childrenByName[@"type"].value;
+					[self addPropertiesFrom:referencedNode additionalRequired:node.requiredProperties];
 				}
-
-				property.itemType = itemType;
 			}
-
-			[self.properties addObject:property];
 		}
 	}
 
 	return (self);
+}
+
+
+- (void)addPropertiesFrom:(OCYAMLNode *)node additionalRequired:(NSArray<NSString *> *)additionalRequired
+{
+	for (OCYAMLNode *propertyNode in node.childrenByName[@"properties"].children)
+	{
+		OCSchemaProperty *property = [OCSchemaProperty new];
+
+		property.schema = self;
+		property.yamlNode= propertyNode;
+
+		property.name = propertyNode.name;
+		property.desc = propertyNode.childrenByName[@"description"].value;
+		property.type = propertyNode.childrenByName[@"type"].value;
+		property.required = [node.requiredProperties containsObject:propertyNode.name] || [additionalRequired containsObject:propertyNode.name];
+		property.format = propertyNode.childrenByName[@"format"].value;
+		property.pattern = propertyNode.childrenByName[@"pattern"].value;
+
+		if (property.type == nil)
+		{
+			property.type = propertyNode.childrenByName[@"$ref"].value;
+		}
+
+		if ([property.type isEqual:@"array"] && (propertyNode.childrenByName[@"items"] != nil))
+		{
+			OCSchemaPropertyType itemType = propertyNode.childrenByName[@"items"].childrenByName[@"$ref"].value;
+
+			if (itemType == nil)
+			{
+				itemType = propertyNode.childrenByName[@"items"].childrenByName[@"type"].value;
+			}
+
+			property.itemType = itemType;
+		}
+
+		[self.properties addObject:property];
+	}
 }
 
 @end
